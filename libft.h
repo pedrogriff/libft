7851599a24f1bde0224dc7b58d017fa0073a3a8f
Pedro Griff Marcincowski <marcincowski@gmail.com>
2021-07-29 20:54:20 -0300

    My first header file - WiP

---
+++ libft.h
@@ -0,0 +1,18 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+
+void    ft_bzero(void *s, size_t len);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+void    *ft_memcpy(void *dst, const void *src, size_t len);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+void    *ft_memset(void *block, int pos, size_t size);
+size_t  ft_strlen(const char *s);
+
+#endif
+// http://mindbending.org/pt/bibliotecas-estaticas-simples
