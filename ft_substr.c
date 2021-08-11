a2906ceb97c640d0bdbd09f8f8bbe790fa22c67c
Pedro Griff Marcincowski <marcincowski@gmail.com>
2021-08-11 14:31:35 -0300

    Work with empty strings

--- ft_substr.c
+++ ft_substr.c
@@ -4,11 +4,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
+	size_t	size;
 
-	i = 0;
-	sub = (malloc(len + 1));
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		size = ft_strlen(s) - start + 1;
+	else
+		size = len + 1;
+	sub = malloc(size);
 	if (sub == NULL)
 		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		sub[i] = s[start + i];
